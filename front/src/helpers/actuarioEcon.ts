aa31e7cf06d53f2da0f19c946b274ed223b6ae8e
TDamonteAzpiazu <tobidamonte@gmail.com>
2024-08-13 19:36:56 -0300

    Added helpers with the careers

---
+++ front/src/helpers/actuarioEcon.ts
@@ -0,0 +1,317 @@
+export const actuarioEcon = [
+    {
+        "codigo": 241,
+        "nombre": "Análisis Matemático I",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 242,
+        "nombre": "Economía",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 243,
+        "nombre": "Sociología",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 244,
+        "nombre": "Metodología de las Ciencias Sociales",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 245,
+        "nombre": "Álgebra",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 246,
+        "nombre": "Historia Económica y Social General",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 247,
+        "nombre": "Teoría Contable",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 248,
+        "nombre": "Estadística I",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [241],
+        "esOptativa": false
+    },
+    {
+        "codigo": 249,
+        "nombre": "Historia Económica y Social Argentina",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [242, 244, 246],
+        "esOptativa": false
+    },
+    {
+        "codigo": 261,
+        "nombre": "Teoría Política y Derecho Público",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 262,
+        "nombre": "Macroeconomía I",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [241, 242],
+        "esOptativa": false
+    },
+    {
+        "codigo": 284,
+        "nombre": "Análisis Matemático II",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [241, 245],
+        "esOptativa": false
+    },
+    {
+        "codigo": 273,
+        "nombre": "Instituciones del Derecho Privado",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 276,
+        "nombre": "Cálculo Financiero",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [248],
+        "esOptativa": false
+    },
+    {
+        "codigo": 279,
+        "nombre": "Administración Financiera",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [262, 276],
+        "esOptativa": false
+    },
+    {
+        "codigo": 283,
+        "nombre": "Macroeconomía II",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [262, 288],
+        "esOptativa": false
+    },
+    {
+        "codigo": 285,
+        "nombre": "Estadística II",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [248, 284],
+        "esOptativa": false
+    },
+    {
+        "codigo": 286,
+        "nombre": "Microeconomía II",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [284, 290],
+        "esOptativa": false
+    },
+    {
+        "codigo": 287,
+        "nombre": "Geografía Económica",
+        "cargaHorariaSemanal": 2,
+        "correlativas": [249],
+        "esOptativa": false
+    },
+    {
+        "codigo": 288,
+        "nombre": "Matemática para Economistas",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [284],
+        "esOptativa": false
+    },
+    {
+        "codigo": 289,
+        "nombre": "Epistemología de la Economía",
+        "cargaHorariaSemanal": 2,
+        "correlativas": [244, 262, 290],
+        "esOptativa": false
+    },
+    {
+        "codigo": 290,
+        "nombre": "Microeconomía I (para Economistas)",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [242, 284],
+        "esOptativa": false
+    },
+    {
+        "codigo": 552,
+        "nombre": "Dinero, Crédito y Bancos",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [283, 286, 288],
+        "esOptativa": false
+    },
+    {
+        "codigo": 751,
+        "nombre": "Estadística Actuarial",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [285],
+        "esOptativa": false
+    },
+    {
+        "codigo": 752,
+        "nombre": "Análisis Numérico",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [288],
+        "esOptativa": false
+    },
+    {
+        "codigo": 753,
+        "nombre": "Biometría Actuarial",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [276, 751, 752],
+        "esOptativa": false
+    },
+    {
+        "codigo": 754,
+        "nombre": "Teoría Actuarial de los Seguros Personales",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [753],
+        "esOptativa": false
+    },
+    {
+        "codigo": 755,
+        "nombre": "Teoría Actuarial de los Seguros Patrimoniales",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [276, 751, 752],
+        "esOptativa": false
+    },
+    {
+        "codigo": 756,
+        "nombre": "Teoría Actuarial de los Fondos y Planes de Jubilaciones, Pensiones y Salud",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [754],
+        "esOptativa": false
+    },
+    {
+        "codigo": 757,
+        "nombre": "Teoría de Equilibrio Actuarial",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [754, 755],
+        "esOptativa": false
+    },
+    {
+        "codigo": 758,
+        "nombre": "Bases Actuariales de las Inversiones y Financiaciones",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [276, 751],
+        "esOptativa": false
+    },
+    {
+        "codigo": 759,
+        "nombre": "Seminario de Integración y Aplicación",
+        "cargaHorariaSemanal": 0,
+        "correlativas": [754, 755, 756, 757, 758],
+        "esOptativa": false
+    },
+    {
+        "codigo": 721,
+        "nombre": "Poder Económico y Derechos Humanos",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": true
+    },
+    {
+        "codigo": 724,
+        "nombre": "Economía de la Seguridad Social",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": true
+    },
+    {
+        "codigo": 786,
+        "nombre": "Derecho de Seguros",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": true
+    },
+    {
+        "codigo": 551,
+        "nombre": "Econometría (vigente en 2024)",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [],
+        "esOptativa": true
+    },
+    {
+        "codigo": 717,
+        "nombre": "Modelos y Proyecciones Actuariales (vigente en 2024)",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": true
+    },
+    {
+        "codigo": 720,
+        "nombre": "Demografía y Seguridad Social (vigente en 2024)",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [753],
+        "esOptativa": true
+    },
+    {
+        "codigo": 741,
+        "nombre": "Elementos de Topología para Economía y Gestión (vigente en 2024)",
+        "cargaHorariaSemanal": 2,
+        "correlativas": [288],
+        "esOptativa": true
+    },
+    {
+        "codigo": 746,
+        "nombre": "Computación Científica Actuarial (vigente en 2024)",
+        "cargaHorariaSemanal": 0,
+        "correlativas": [],
+        "esOptativa": true
+    },
+    {
+        "codigo": 750,
+        "nombre": "Métodos Cuantitativos (vigente en 2024)",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": true
+    },
+    {
+        "codigo": 763,
+        "nombre": "Teoría de los Juegos (vigente en 2024)",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": true
+    },
+    {
+        "codigo": 784,
+        "nombre": "Análisis Numérico Avanzado (vigente en 2024)",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": true
+    },
+    {
+        "codigo": 785,
+        "nombre": "Cálculo Financiero -Nuevo Enfoque- (vigente en 2024)",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": true
+    },
+    {
+        "codigo": 791,
+        "nombre": "Seminario de Ética de las Ocupaciones (vigente en 2024)",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": true
+    }
+]
---
+++ front/src/helpers/sistemas.ts
@@ -0,0 +1,261 @@
+export const sistemas = [
+    {
+        "codigo": 241,
+        "nombre": "Análisis Matemático I",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 242,
+        "nombre": "Economía",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 243,
+        "nombre": "Sociología",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 244,
+        "nombre": "Metodología de las Ciencias Sociales",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 245,
+        "nombre": "Álgebra",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 246,
+        "nombre": "Historia Económica y Social General",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 247,
+        "nombre": "Teoría Contable",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [241, 242, 243, 244, 245, 246],
+        "esOptativa": false
+    },
+    {
+        "codigo": 248,
+        "nombre": "Estadística I",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [241, 242, 243, 244, 245, 246],
+        "esOptativa": false
+    },
+    {
+        "codigo": 249,
+        "nombre": "Historia Económica y Social Argentina",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [241, 242, 243, 244, 245, 246],
+        "esOptativa": false
+    },
+    {
+        "codigo": 250,
+        "nombre": "Microeconomía I",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [241, 242, 243, 244, 245, 246],
+        "esOptativa": false
+    },
+    {
+        "codigo": 252,
+        "nombre": "Administración General",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [241, 242, 243, 244, 245, 246],
+        "esOptativa": false
+    },
+    {
+        "codigo": 1275,
+        "nombre": "Introducción a la Tecnología de la Información y las Comunicaciones",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [241, 242, 243, 244, 245, 246],
+        "esOptativa": false
+    },
+    {
+        "codigo": 274,
+        "nombre": "Sistemas Administrativos",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [252],
+        "esOptativa": false
+    },
+    {
+        "codigo": 276,
+        "nombre": "Cálculo Financiero",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [248],
+        "esOptativa": false
+    },
+    {
+        "codigo": 277,
+        "nombre": "Gestión y Costos",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [247],
+        "esOptativa": false
+    },
+    {
+        "codigo": 278,
+        "nombre": "Macroeconomía y Política Económica",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 279,
+        "nombre": "Administración Financiera",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [276],
+        "esOptativa": false
+    },
+    {
+        "codigo": 452,
+        "nombre": "Sociología de la Organización",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [252],
+        "esOptativa": false
+    },
+    {
+        "codigo": 457,
+        "nombre": "Teoría de la Decisión",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [277],
+        "esOptativa": false
+    },
+    {
+        "codigo": 651,
+        "nombre": "Lógica",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [1275],
+        "esOptativa": false
+    },
+    {
+        "codigo": 655,
+        "nombre": "Tecnología de las Comunicaciones",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [1653],
+        "esOptativa": false
+    },
+    {
+        "codigo": 657,
+        "nombre": "Sistemas de Datos",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [651],
+        "esOptativa": false
+    },
+    {
+        "codigo": 658,
+        "nombre": "Metodología de los Sistemas de Información",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [1275],
+        "esOptativa": false
+    },
+    {
+        "codigo": 740,
+        "nombre": "Redes Informáticas",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [655],
+        "esOptativa": false
+    },
+    {
+        "codigo": 1601,
+        "nombre": "Ingeniería en Software",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [1275],
+        "esOptativa": false
+    },
+    {
+        "codigo": 1603,
+        "nombre": "Derecho Informático I",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": false
+    },
+    {
+        "codigo": 1604,
+        "nombre": "Derecho Informático II",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [1603],
+        "esOptativa": false
+    },
+    {
+        "codigo": 1652,
+        "nombre": "Teoría de los Lenguajes y Algoritmos",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [651],
+        "esOptativa": false
+    },
+    {
+        "codigo": 1653,
+        "nombre": "Tecnología de los Computadores y Sistemas Operativos",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [1275],
+        "esOptativa": false
+    },
+    {
+        "codigo": 1654,
+        "nombre": "Construcción de Aplicaciones Informáticas",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [1652],
+        "esOptativa": false
+    },
+    {
+        "codigo": 1659,
+        "nombre": "Auditoría y Seguridad de los Sistemas de Información",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [657, 658],
+        "esOptativa": false
+    },
+    {
+        "codigo": 1660,
+        "nombre": "Actuación Profesional del Licenciado en Sistemas de las Organizaciones",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [740, 1654],
+        "esOptativa": false
+    },
+    {
+        "codigo": 1799,
+        "nombre": "Gestión de Recursos Informáticos",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [1601, 1659],
+        "esOptativa": false
+    },
+    {
+        "codigo": 459,
+        "nombre": "Dirección General (vigente en 2024)",
+        "cargaHorariaSemanal": 6,
+        "correlativas": [],
+        "esOptativa": true
+    },
+    {
+        "codigo": 716,
+        "nombre": "Gestión de la Innovación y Tecnología Informática (vigente en 2024)",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": true
+    },
+    {
+        "codigo": 739,
+        "nombre": "Comercio Electrónico (vigente en 2024)",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": true
+    },
+    {
+        "codigo": 1715,
+        "nombre": "Gobierno de Datos, Analítica e Inteligencia de Negocios (vigente en 2024)",
+        "cargaHorariaSemanal": 4,
+        "correlativas": [],
+        "esOptativa": true
+    }
+]

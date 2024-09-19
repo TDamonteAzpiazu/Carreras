bef4b2714b91531217e780bb71185cee97c5803b
TDamonteAzpiazu <tobidamonte@gmail.com>
2024-09-19 10:58:47 -0300

    Revert "Reapply "deploy prueba""
    This reverts commit ff02df770bc784eef5403fdfd446b43a09743023.

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -4,16 +4,16 @@ import './reset.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 

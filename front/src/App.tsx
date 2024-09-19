499563a01996b91b6da2049621b7756430f2287a
TDamonteAzpiazu <tobidamonte@gmail.com>
2024-09-19 10:33:09 -0300

    Cositas para el deploy

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { Navbar } from './components/primary/navbar/navbar';
 import { Footer } from './components/primary/footer/footer';
-import { Route, Routes } from 'react-router-dom';
 import { Home } from './views/home/home';
 import { Sistemas } from './views/carreras/sistemas/sistemas';
 import { Admin } from './views/carreras/admin/admin';
@@ -9,24 +8,27 @@ import { Contador } from './views/carreras/contador/contador';
 import { ActEcon } from './views/carreras/actEcon/actEcon';
 import { ActAdmin } from './views/carreras/actAdmin/actAdmin';
 import { Economia } from './views/carreras/economia/economia';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="todo">
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sistemas" element={<Sistemas />} />
-          <Route path="/actuarioEconomia" element={<ActEcon />} />
-          <Route path="/actuarioAdministracion" element={<ActAdmin />} />
-          <Route path="/economia" element={<Economia />} />
-          <Route path="/administracion" element={<Admin />} />
-          <Route path="/contador" element={<Contador />} />
-        </Routes>
+    <Router>
+      <div className="todo">
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sistemas" element={<Sistemas />} />
+            <Route path="/actuarioEconomia" element={<ActEcon />} />
+            <Route path="/actuarioAdministracion" element={<ActAdmin />} />
+            <Route path="/economia" element={<Economia />} />
+            <Route path="/administracion" element={<Admin />} />
+            <Route path="/contador" element={<Contador />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
